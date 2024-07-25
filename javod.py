8f7ceeef1613dcf7a30de2a38725bb8f2eb28279
ZXCuan <72718264+SFGamink@users.noreply.github.com>
2024-07-25 10:45:04 +0700

    Update javod.py
    Tambahan Fitur Auto Upgrade & Auto Clear Task

--- javod.py
+++ javod.py
@@ -2,6 +2,7 @@ import requests
 from colorama import Fore, Style, init
 from datetime import datetime, timedelta
 import time
+import os
 
 # Inisialisasi colorama untuk sistem operasi Windows
 init()
@@ -101,15 +102,18 @@ def claim_farm(last_auth_str, telegram_init_data):
                 break  # Keluar dari loop setelah berhasil klaim
             else:
                 print(Fore.RED + Style.BRIGHT + f'Gagal mengirim permintaan POST. Kode status: {response.status_code}')
+                # Langsung menuju akun berikutnya setelah gagal klaim
+                break
 
         except requests.RequestException as e:
             print(Fore.RED + Style.BRIGHT + f'Gagal mengirim permintaan POST: {str(e)}')
+            # Langsung menuju akun berikutnya setelah gagal klaim
+            break
 
         except Exception as e:
             print(Fore.RED + Style.BRIGHT + f'Unexpected error: {str(e)}')
-
-        # Menunggu 2 jam sebelum mencoba klaim kembali setelah kegagalan atau setelah berhasil klaim
-        time.sleep(delay_hours * 3600)  # 2 jam = 2 * 3600 detik
+            # Langsung menuju akun berikutnya setelah gagal klaim
+            break
 
 def read_telegram_init_data(init_file):
     """Fungsi untuk membaca semua data inisialisasi dari file."""
@@ -122,11 +126,140 @@ def read_telegram_init_data(init_file):
         print(Fore.RED + Style.BRIGHT + f"File '{init_file}' tidak ditemukan.")
     except Exception as e:
         print(Fore.RED + Style.BRIGHT + f"Error saat membaca file '{init_file}': {str(e)}")
-    return telegram_init_data_list        
+    return telegram_init_data_list
+
+def auto_upgrade(endpoint):
+    """Fungsi untuk melakukan auto upgrade pada toolkit atau workbench."""
+    # Define the URL based on the endpoint parameter
+    if endpoint == "Toolkit":
+        url = "https://zavod-api.mdaowallet.com/user/upgradeToolkit"
+    elif endpoint == "Workbench":
+        url = "https://zavod-api.mdaowallet.com/user/upgradeWorkbench"
+    else:
+        print("Invalid endpoint specified.")
+        return
+    
+    telegram_init_data_list = read_telegram_init_data('init.txt')
+    
+    if telegram_init_data_list:
+        for telegram_init_data in telegram_init_data_list:
+            headers['telegram-init-data'] = telegram_init_data
+            
+            try:
+                response = requests.post(url, headers=headers)
+                response.raise_for_status()
+                
+                # Cek status respons untuk menentukan pesan yang sesuai
+                if response.status_code == 200:
+                    print(Fore.GREEN + f"upgrade {endpoint} successful!")
+                else:
+                    print(Fore.RED + f"upgrade {endpoint} failed. HTTP status: {response.status_code}")
+            
+            except requests.exceptions.RequestException as e:
+                print(Fore.RED + f"upgrade {endpoint} failed. Duit lu kurang !!!")
+            
+            except Exception as e:
+                print(f"Unexpected error during upgrade {endpoint} : {e}")
+            
+            # Add a short delay between upgrades
+            time.sleep(5)  # Adjust as needed
+
+def confirm_auto_upgrade():
+    """Fungsi untuk konfirmasi pengguna sebelum melakukan auto upgrade."""
+    while True:
+        choice = input(Fore.BLACK + f"Apakah Anda ingin melakukan auto upgrade? (y/n): ").strip().lower()
+        if choice == 'y':
+            auto_upgrade("Toolkit")
+            auto_upgrade("Workbench")
+            break
+        elif choice == 'n':
+            break
+        else:
+            print("Pilihan tidak valid. Harap masukkan 'y' atau 'n'.")
+
+def get_task(telegram_init_data):
+    url = "https://zavod-api.mdaowallet.com/missions"
+    params = {
+        "offset": 0,
+        "status": "ACTIVE"
+    }
+    headers['telegram-init-data'] = telegram_init_data
+    
+    try:
+        response = requests.get(url, params=params, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
+        return None
+
+def select_and_claim_tasks(telegram_init_data):
+    tasks = get_task(telegram_init_data)
+    if tasks and tasks.get("missions"):
+        for task in tasks["missions"]:
+            mission_id = task["id"]
+            print(f"Selecting and claiming task with ID: {mission_id}")
+            if task["type"] == "LINK":
+                click_link_task(mission_id, telegram_init_data)
+            select_task(mission_id, telegram_init_data)
+            claim_task(mission_id, telegram_init_data)
+    else:
+        print(Fore.RED + f"Tidak ada task")
+
+def select_task(mission_id, telegram_init_data):
+    url = f"https://zavod-api.mdaowallet.com/missions/confirm/telegram/{mission_id}"
+    headers['telegram-init-data'] = telegram_init_data
+    
+    payload = {
+        "state": "COMPLETED",
+        "status": "COMPLETED"
+    }
+    
+    try:
+        response = requests.post(url, headers=headers, json=payload)
+        response.raise_for_status()
+        print(Fore.GREEN + f"Task {mission_id} confirmed successfully.")
+    except requests.exceptions.RequestException as e:
+        print(Fore.RED + f"Error occurred while confirming task {mission_id}: {e}")
+
+def click_link_task(mission_id, telegram_init_data):
+    url = f"https://zavod-api.mdaowallet.com/missions/confirm/link/{mission_id}"
+    headers['telegram-init-data'] = telegram_init_data
+    
+    payload = {
+        "state": "CLICKED",
+        "status": "CLICKED"
+    }
+    
+    try:
+        response = requests.post(url, headers=headers, json=payload)
+        response.raise_for_status()
+        print(Fore.GREEN + f"Clicked link for task {mission_id} successfully.")
+    except requests.exceptions.RequestException as e:
+        print(Fore.RED + f"Error occurred while clicking link for task {mission_id}: {e}")
+
+def claim_task(task_id, telegram_init_data):
+    url = f"https://zavod-api.mdaowallet.com/missions/claim/{task_id}"
+    headers['telegram-init-data'] = telegram_init_data
+    
+    try:
+        response = requests.post(url, headers=headers)
+        response.raise_for_status()
+        
+        if response.status_code == 200:
+            print(Fore.GREEN + f"Task {task_id} claimed successfully.")
+        else:
+            print(Fore.RED + f"Failed to claim task {task_id}. Status code: {response.status_code}")
+            
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred while claiming task {task_id}: {e}")            
+
+def clear_screen():
+    """Fungsi untuk membersihkan layar konsol."""
+    os.system('cls' if os.name == 'nt' else 'clear')        
 
 def main():
-    """Fungsi utama untuk menjalankan bot."""
-    print_welcome_message()
 
     # Baca semua data inisialisasi dari init.txt
     telegram_init_data_list = read_telegram_init_data('init.txt')
@@ -135,6 +268,17 @@ def main():
         idx = 0
         while idx < len(telegram_init_data_list):
             telegram_init_data = telegram_init_data_list[idx]
+
+            """Fungsi utama untuk menjalankan bot."""
+            print_welcome_message()
+
+            # Konfirmasi Auto Upgrade
+            confirm_auto_upgrade()
+
+            # Konfirmasi Auto Clear Task
+            clear_auto_task = input(Fore.BLACK + "Apakah Anda ingin auto clear task ? (y/n): ").strip().lower()
+            if clear_auto_task == 'y':
+                select_and_claim_tasks(telegram_init_data)            
 
             # Mengambil data pengguna dari API untuk akun tertentu
             user_data = fetch_user_data(telegram_init_data)
@@ -173,12 +317,15 @@ def main():
             # Menunggu 5 menit sebelum melanjutkan ke akun berikutnya
             if idx < len(telegram_init_data_list) - 1:
                 print(f"Menunggu 5 menit sebelum melanjutkan ke akun {idx + 2}...")
-                time.sleep(300)  # 300 detik = 5 menit
+                time.sleep(5)  # 300 detik = 5 menit
             else:
-                print("Ini adalah akun terakhir dalam daftar. Program akan berhenti setelah klaim terakhir.")
+                print("Ini adalah akun terakhir dalam daftar. Program akan berhenti setelah klaim terakhir.")    
 
             # Increment index untuk mengambil akun selanjutnya di iterasi berikutnya
             idx += 1
+
+            # Untuk membersihkan tampilan
+            clear_screen()
     else:
         print(Fore.RED + Style.BRIGHT + "Tidak ada data inisialisasi yang dibaca dari 'init.txt'.")
 
